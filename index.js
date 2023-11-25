4e26b69b0284eb5fe9306dedbb1994200c004737
elegantjump <viktor050408@gmail.com>
2023-11-25 09:03:11 +0200

    r

---
+++ index.js
@@ -0,0 +1,28 @@
+const result = 5+5+'5';
+console.log(result);
+console.log(typeof result)
+// 1
+ 
+
+let email = "ktoto@gmail.com";
+console.log(email.length)
+console.log(email.includes("@"))
+let email1 = "ktoto@gmail.com".length;
+console.log(email1)
+// 2
+
+
+
+
+let My = 'My';
+let name = 'name';
+let is = 'is';
+let Fullname = My+name+is;
+console.log(Fullname+'Viktor');
+//3
+
+
+
+let username = "Artem";
+let payment = 300;
+alert("Дякуємо" + " " + username + " з вас" + " " + payment + " " + "гривень");
